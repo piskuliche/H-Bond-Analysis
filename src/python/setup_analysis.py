a30983164368172a8ba9821ec3910826b8b5311e
Zeke Piskulich <piskulichz@gmail.com>
2023-06-04 16:39:10 -0700

    update

--- src/python/setup_analysis.py
+++ src/python/setup_analysis.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+def Read_Hyd_File(filename):
+    data = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        for line in lines[2:-1]:
+            data.append(line.strip().split())
+    return np.as_array(data,dtype=float)
+
+
+
 def Pull_Hydration_Data(components=["STYRR","POPC","CLA","SOD","TIP3","LAUR"], to_match=["C","P","N","O"],
                         file_start=1, file_stop=100, fail_okay=False):
     """This is a python function for pulling the data from a hydration calculation
@@ -8,4 +18,5 @@ def Pull_Hydration_Data(components=["STYRR","POPC","CLA","SOD","TIP3","LAUR"], t
         
     """
 
-    
+    for hyd_dir in range(file_start, file_stop):
+        Read_Hyd_File("")
