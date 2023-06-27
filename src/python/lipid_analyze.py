930f744b4777830abc9f1a3cea28b5643e9ba139
Zeke Piskulich <piskulichz@gmail.com>
2023-06-27 11:03:11 -0700

    updated angles

--- src/python/lipid_analyze.py
+++ src/python/lipid_analyze.py
@@ -60,7 +60,7 @@ def Calc_Z_byres(u, ts, AtomGroup, ResAtoms, optional = None):
     if optional != None:
         raise ValueError("Optional must be None for Calc_Z_byres")
     zcom = AtomGroup.center_of_mass()[2]
-    zdist = np.abs(ResAtoms.center_of_mass(wrap=True)[2] - zcom)
+    zdist = ResAtoms.center_of_mass(wrap=True)[2] - zcom
     return zdist
 
 def Calc_Rg_byres(u, ts, AtomGroup, ResAtoms, optional = None):
@@ -76,9 +76,20 @@ def Calc_P2_byres(u, ts, AtomGroup, ResAtoms, optional = [0,0,1]):
     pos1 = ResAtoms.positions[0]
     pos2 = ResAtoms.positions[1]
     bond_vec = bond_unit_vec(pos1, pos2, L)
-    angle = np.arccos(np.dot(bond_vec, optional))
-    p2_value = P2(angle)
+    cosangle = np.dot(bond_vec, optional)
+    p2_value = P2(cosangle)
     return p2_value
+
+def Calc_CosAngle_byres(u, ts, AtomGroup, ResAtoms, optional = [0,0,1]):
+    if len(ResAtoms) != 2:
+        print("ResAtoms must be only two atoms")
+
+    L = ts.dimensions[:3]
+    pos1 = ResAtoms.positions[0]
+    pos2 = ResAtoms.positions[1]
+    bond_vec = bond_unit_vec(pos1, pos2, L)
+    cosangle = np.dot(bond_vec, optional)
+    return cosangle
 
 def Calc_Angle_byres(u, ts, AtomGroup, ResAtoms, optional = [0,0,1]):
     if len(ResAtoms) != 2:
@@ -88,7 +99,8 @@ def Calc_Angle_byres(u, ts, AtomGroup, ResAtoms, optional = [0,0,1]):
     pos1 = ResAtoms.positions[0]
     pos2 = ResAtoms.positions[1]
     bond_vec = bond_unit_vec(pos1, pos2, L)
-    angle = np.arccos(np.dot(bond_vec, optional))
+    cosangle = np.dot(bond_vec, optional)
+    angle = np.arccos(cosangle)
     return angle
 
 def Main_Analysis(output_data, tprfile, trajfile):
@@ -105,13 +117,17 @@ def Main_Analysis(output_data, tprfile, trajfile):
         output_data["zdists"].append(MolData(zmol, zdat, zmol))
         rmol, rdat = Analyze_by_Residue(u, ts, membrane, Calc_Rg_byres)
         output_data["rg"].append(MolData(rmol, rdat, zmol))
-        pmol, pdat = Analyze_by_Residue(u, ts, laur_CN, Calc_Angle_byres, optional=[0,0,1])
-        output_data["tiltangle"].append(MolData(pmol,pdat, zmol))
+        pmol, pdat = Analyze_by_Residue(u, ts, laur_CN, Calc_P2_byres, optional=[0,0,1])
+        output_data["tiltp2"].append(MolData(pmol,pdat, zmol))
+        cmol, cdat = Analyze_by_Residue(u, ts, laur_CN, Calc_CosAngle_byres, optional=[0,0,1])
+        output_data["tiltcosangle"].append(MolData(cmol,cdat, zmol))
+        amol, adat = Analyze_by_Residue(u, ts, laur_CN, Calc_Angle_byres, optional=[0,0,1])
+        output_data["tiltangle"].append(MolData(amol,adat, zmol))
 
     return output_data
 
 def Do_Files(start=1,stop=100):
-    output_data = {"zdists":[], "rg":[], "tiltangle":[]}
+    output_data = {"zdists":[], "rg":[], "tiltangle":[], "tiltcosangle":[], "tiltp2":[]}
     for i in range(start,stop):
         tprfile = "../../tpr/step7_%d.tpr"%i
         xtcfile = "../../xtc/step7_%d.xtc"%i
@@ -120,7 +136,7 @@ def Do_Files(start=1,stop=100):
 def Do_File(filenumber=1):
     tprfile = "../../tpr/step7_%d.tpr"%filenumber
     xtcfile = "../../xtc/step7_%d.xtc"%filenumber
-    output_data = {"zdists":[], "rg":[], "tiltangle":[]}
+    output_data = {"zdists":[], "rg":[], "tiltangle":[], "tiltcosangle":[], "tiltp2":[]}
     output_data = Main_Analysis(output_data, tprfile, xtcfile)
     Return_Data(output_data, filenumber)
 
