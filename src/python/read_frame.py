8b2db2f8077f571b8a1afec9e8771a5788e850de
Zeke Piskulich <piskulichz@gmail.com>
2023-06-28 07:59:40 -0400

    Basic refactoring and added other angle calcs

--- src/python/read_frame.py
+++ src/python/read_frame.py
@@ -11,12 +11,19 @@ It assumes that you have stored hydration data, hydrogen bonding data,
 voronoi data, as well as lipid/laurdan conformation data in the appropriate formats (more the other class)
 ********************************************
 """
-class frame_data():
 
-    def __init__(self, nmols, ncomps=[180,20], compno=[2,6]):
+
+class frame_data():
+    """
+    This class stores the data for a single frame of the calculation.
+    It assumes that you have stored hydration data, hydrogen bonding data, 
+    voronoi data, as well as lipid/laurdan conformation data in the appropriate formats (more the other class)
+    """
+
+    def __init__(self, nmols, ncomps=[180, 20], compno=[2, 6]):
         self.components = [2] * ncomps[0] + [6] * ncomps[1]
         self.nmols = nmols
-        
+
         # Voronoi Data
         self.data = {}
         self.data["hyd_mol"] = np.zeros(nmols)
@@ -29,14 +36,14 @@ class frame_data():
         self.compno = compno
         self.ncomps = ncomps
         return
-    
+
     def add_data(self, dataname, data):
         """ Adds data to the frame.
 
         Args:
             dataname (str): The name of the data to be added.
             data (float(s)): The data to be added.
-        
+
         Returns:
             None
         """
@@ -44,8 +51,8 @@ class frame_data():
             self.data[dataname] = np.zeros(self.nmols)
         for i, d in enumerate(data):
             self.data[dataname][i] = d
-        return 
-    
+        return
+
     def grab_data(self, dataname, occupancy=False):
         """ Returns the data from the frame.
 
@@ -68,8 +75,7 @@ class frame_data():
             return occdata0, occdata1, occdata2, occdata3
         else:
             return self.data[dataname]
-        
-    
+
     def query_average(self, dataname, molec=2, occupancy=False):
         """ Returns the average of the data in the frame.
 
@@ -89,13 +95,13 @@ class frame_data():
                     avs.append(np.average(data[i]))
                 else:
                     avs.append(0)
-                
+
             return avs
         else:
             return np.average(self.grab_data(dataname, molec=molec))
-        
-    def histogram_frame(self, dataname, molec=2, bins=50, range=(0,100), 
-                        bin_edges = None, occupancy=False):
+
+    def histogram_frame(self, dataname, molec=2, bins=50, range=(0, 100),
+                        bin_edges=None, occupancy=False):
         """
         """
         data = self.grab_data(dataname, occupancy=occupancy)[molec]
@@ -104,7 +110,7 @@ class frame_data():
         else:
             hist, _ = np.histogram(data, bins=bin_edges)
         return hist, bin_edges
-    
+
     def add_note(self, note):
         """ Adds a note to the frame.
 
@@ -132,21 +138,22 @@ The key dependency is that it requires access to the frame_data class.
 ********************************************
 """
 
+
 class calculation_data:
 
-    def __init__(self, dirframes=501, dirstart=1, dirstop=100, ncomps=[180,2], compno=[2,6]):
+    def __init__(self, dirframes=501, dirstart=1, dirstop=100, ncomps=[180, 2], compno=[2, 6]):
         """ Initializes the class
         """
         self.dirframes = dirframes
-        self.dirstart  = dirstart
-        self.dirstop   = dirstop
-        self.ncomps    = ncomps
-        self.compno    = compno
-        self.nmols     = sum(ncomps)
-        self.frames    = []
+        self.dirstart = dirstart
+        self.dirstop = dirstop
+        self.ncomps = ncomps
+        self.compno = compno
+        self.nmols = sum(ncomps)
+        self.frames = []
         self.build_data()
         return
-    
+
     def build_data(self):
         """ Builds the data from the directories.
 
@@ -168,15 +175,16 @@ class calculation_data:
 
         Args:
             None
-        
+
         Returns:
             None
 
         """
         for frame in range(self.dirframes):
-            self.frames.append(frame_data(self.nmols, self.ncomps, self.compno))
+            self.frames.append(frame_data(
+                self.nmols, self.ncomps, self.compno))
         return
-    
+
     def pull_lipid_data(self, ndir):
         """ Pulls data from the lipid calculations
 
@@ -185,15 +193,31 @@ class calculation_data:
         Returns:
 
         """
-        zdists = pickle.load(open("%d/output_zdists_%d.pckl" % (ndir,ndir), 'rb'))
-        rg = pickle.load(open("%d/output_rg_%d.pckl" % (ndir,ndir), 'rb'))
-        p2 = pickle.load(open("%d/output_laurp2_%d.pckl" % (ndir,ndir), 'rb'))
-        angle = pickle.load(open("%d/output_tiltangle_%d.pckl" % (ndir,ndir), 'rb'))
+        zdists = pickle.load(
+            open("%d/output_zdists_%d.pckl" % (ndir, ndir), 'rb'))
+        rg = pickle.load(
+            open("%d/output_rg_%d.pckl" % (ndir, ndir), 'rb'))
+        p2 = pickle.load(
+            open("%d/output_laurp2_%d.pckl" % (ndir, ndir), 'rb'))
+        angle = pickle.load(
+            open("%d/output_tiltangle_%d.pckl" % (ndir, ndir), 'rb'))
+        cosangle = pickle.load(
+            open("%d/output_tiltcosangle_%d.pckl" % (ndir, ndir), 'rb'))
+        tiltp2 = pickle.load(
+            open("%d/output_tiltp2_%d.pckl" % (ndir, ndir), 'rb'))
         for frame in range(self.dirframes):
-            self.frames[frame+(ndir-1)*self.dirframes].add_data("zdists", zdists[frame])
-            self.frames[frame+(ndir-1)*self.dirframes].add_data("rg", rg[frame])
-            self.frames[frame+(ndir-1)*self.dirframes].add_data("laurtilt", p2[frame])
-            self.frames[frame+(ndir-1)*self.dirframes].add_data("tiltangle", angle[frame])
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("zdists", zdists[frame])
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("rg", rg[frame])
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("laurtilt", p2[frame])
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("tiltangle", angle[frame])
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("tiltcosangle", cosangle[frame])
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("tiltp2", tiltp2[frame])
         return
 
     def pull_dir_hyd(self, ndir):
@@ -215,9 +239,10 @@ class calculation_data:
             hyd_data = np.array([])
             for comp in self.compno:
                 hyd_data = np.append(hyd_data, hyd_comp[comp][frame])
-            self.frames[frame+(ndir-1)*self.dirframes].add_data("hyd_mol", hyd_data)
+            self.frames[frame+(ndir-1) *
+                        self.dirframes].add_data("hyd_mol", hyd_data)
         return
-    
+
     def pull_voronoi_data(self, ndir):
         """The Voronoi data is stored as a pickle file which is segregated by frame and leaf.
 
@@ -234,7 +259,8 @@ class calculation_data:
 
         """
         try:
-            vor_data = pickle.load(open('../voronoi_plots/data/vor_%d.pckl' % ndir, 'rb'))
+            vor_data = pickle.load(
+                open('../voronoi_plots/data/vor_%d.pckl' % ndir, 'rb'))
         except FileNotFoundError:
             print("No voronoi data for directory %d" % ndir)
             for frame in range(self.dirframes):
@@ -244,25 +270,27 @@ class calculation_data:
                 self.frames[frame_index].add_data("areas", np.zeros(mols))
                 self.frames[frame_index].add_data("leaflet", np.zeros(mols))
                 self.frames[frame_index].add_data("molids", np.zeros(mols))
-                self.frames[frame_index].add_note("voronoi: No voronoi data for directory %d" % ndir)
+                self.frames[frame_index].add_note(
+                    "voronoi: No voronoi data for directory %d" % ndir)
             return
         for frame in range(self.dirframes):
             # Choose the correct frame
             frame_index = frame + (ndir-1) * self.dirframes
 
             # Pull the data from the pickle
-            print("fr",frame, ndir)
+            print("fr", frame, ndir)
             leaf1 = vor_data[frame_index][0]
             leaf2 = vor_data[frame_index][1]
-            
+
             # Geneate the leaflet array 0 for one leaflet, 1 for the other.
-            l1s, l2s = np.zeros(len(leaf1['molids'])), np.ones(len(leaf2['molids']))
+            l1s, l2s = np.zeros(len(leaf1['molids'])), np.ones(
+                len(leaf2['molids']))
 
             # Concatenate the data into a single array
-            molids  = np.concatenate((leaf1['molids'],leaf2['molids']))
-            leafs   = np.concatenate((l1s,l2s))
-            areas   = np.concatenate((leaf1['areas'],leaf2['areas']))
-            occ     = np.concatenate((leaf1['occupancy'],leaf2['occupancy']))
+            molids = np.concatenate((leaf1['molids'], leaf2['molids']))
+            leafs = np.concatenate((l1s, l2s))
+            areas = np.concatenate((leaf1['areas'], leaf2['areas']))
+            occ = np.concatenate((leaf1['occupancy'], leaf2['occupancy']))
 
             # Sort the data by molecule id
             sort_indices = np.argsort(molids)
@@ -273,9 +301,8 @@ class calculation_data:
             self.frames[frame_index].add_data("leaflet", leafs[sort_indices])
             self.frames[frame_index].add_data("molids", molids[sort_indices])
 
-
         return
-    
+
     def hyd_molar_data(self, ndir, comp):
         """ Pulls the hyd molar data from the directory.
 
@@ -287,26 +314,30 @@ class calculation_data:
             np.array: The hyd molar data for the component.
 
         """
-        hyd = np.loadtxt('%d/hyd_molar_%d.dat' % (ndir, comp) , usecols=(1), unpack=True)
+        hyd = np.loadtxt('%d/hyd_molar_%d.dat' %
+                         (ndir, comp), usecols=(1), unpack=True)
         return np.array_split(hyd, self.dirframes)
-    
+
     def analyze_data(self, dataname, start=None, stop=None, occupancy=False):
         if start == None:
             start = self.dirstart
         if stop == None:
             stop = self.dirstop
 
-    
     def grab_frame_data(self, dataname, start, stop, molec=2, occupancy=False, flatten=False, average=False):
         if flatten == True and average == True:
             raise ValueError("Cannot flatten and average data.")
         if occupancy:
             data1, data2, data3, data4 = [], [], [], []
             for frame in range(start, stop):
-                data1.append(self.frames[frame].grab_data(dataname, occupancy=True)[0])
-                data2.append(self.frames[frame].grab_data(dataname, occupancy=True)[1])
-                data3.append(self.frames[frame].grab_data(dataname, occupancy=True)[2])
-                data4.append(self.frames[frame].grab_data(dataname, occupancy=True)[3])
+                data1.append(self.frames[frame].grab_data(
+                    dataname, occupancy=True)[0])
+                data2.append(self.frames[frame].grab_data(
+                    dataname, occupancy=True)[1])
+                data3.append(self.frames[frame].grab_data(
+                    dataname, occupancy=True)[2])
+                data4.append(self.frames[frame].grab_data(
+                    dataname, occupancy=True)[3])
             if flatten:
                 # flatten arrays
                 data1 = [item for sublist in data1 for item in sublist]
@@ -330,7 +361,7 @@ class calculation_data:
                 data = np.mean(data, axis=0)
             return np.array(data)
 
-    def Histogram_Data(self, dataname, molec=2, bins=50, range=(0,100), occupancy=False, frame_start=None, frame_stop=None):
+    def Histogram_Data(self, dataname, molec=2, bins=50, range=(0, 100), occupancy=False, frame_start=None, frame_stop=None):
         """ Generates a histogram of the data for the entire simulation
 
         Args:
@@ -350,24 +381,21 @@ class calculation_data:
         for frame in self.frames[frame_start:frame_stop]:
             ht, bt = None, None
             if ct == 0:
-                ht, bt  = frame.histogram_frame(dataname, molec=molec, bins=bins, range=range, 
-                        bin_edges = None, occupancy=occupancy)
+                ht, bt = frame.histogram_frame(dataname, molec=molec, bins=bins, range=range,
+                                               bin_edges=None, occupancy=occupancy)
                 bin_edges = bt
             else:
-                ht, bt = frame.histogram_frame(dataname, molec=molec, bins=bins, range=range, 
-                        bin_edges = bin_edges, occupancy=occupancy)
+                ht, bt = frame.histogram_frame(dataname, molec=molec, bins=bins, range=range,
+                                               bin_edges=bin_edges, occupancy=occupancy)
             hist.append(ht)
-            
+
             ct = ct + 1
         final_hist = np.sum(np.array(hist), axis=0)
         return final_hist, bin_edges
 
+
 if __name__ == "__main__":
-    data = calculation_data(dirframes=501, dirstart=1, dirstop=100, ncomps=[180,20], compno=[2,6])
-    
+    data = calculation_data(dirframes=501, dirstart=1,
+                            dirstop=100, ncomps=[180, 20], compno=[2, 6])
+
     pickle.dump(data, open('calc_data.pckl', 'wb'))
-
-    
-
-
-
